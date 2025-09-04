dbf1142e552bfbc36732f8ae7d1b7fef5e622114
AtellaThiago <160648495+ThiagoAtella@users.noreply.github.com>
2025-09-04 08:07:48 -0300

    config novo banco

--- SistemaUniversalAcesso/app/src/main/java/com/example/sistemauniversalacesso/database/FirebaseConfig.java
+++ SistemaUniversalAcesso/app/src/main/java/com/example/sistemauniversalacesso/database/FirebaseConfig.java
@@ -1,5 +1,5 @@
 package com.example.sistemauniversalacesso.database;
 
 public class FirebaseConfig {
-    public static final String DATABASE_URL = "https://sua-da6a2-default-rtdb.firebaseio.com";
+    public static final String DATABASE_URL = "https://sua-2899a-default-rtdb.firebaseio.com/";
 }
